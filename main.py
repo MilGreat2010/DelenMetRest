adc8f9f20c494c7aa959bec35c2370991fc7437e
Milan Wiesenekker <57262078+MilGreat2010@users.noreply.github.com>
2021-09-15 16:16:00 +0000

    Added a system where you can not divide by 0

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 def dmr(nummer, deler):
+  stopswitch = 0
   deler = int(deler)
   nummer = int(nummer)
   ateller = 1
   ateller = int(ateller)
   rbereken = deler
   rbereken = int(rbereken)
+  if nummer == 0:
+    print("Kan niet door 0 delen")
+    stopswitch = 1
   while rbereken <= nummer:
+      if stopswitch == 0:
+        if deler == 0:
+          print("Kan niet door 0 delen")
+          stopswitch = 1
+          break
+      else:
+        break
       rbereken = rbereken + deler
       ateller = ateller + 1
   if rbereken > nummer:
       rbereken = rbereken - deler
       ateller = ateller - 1
       rest = nummer - rbereken
-  print ("Rest:", rest)
-  print ("Antwoord:", ateller)
+  if stopswitch == 1:
+      ateller = 0
+      rest = 0
+      stopswitch = 0
+  else:
+    print ("Rest:", rest)
+    print ("Antwoord:", ateller)
